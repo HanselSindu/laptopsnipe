a5cd5afba693dcb2c5b3db1325bae317c8d34340
Hansel Sindu Linata <hansel.sindu@gmail.com>
2017-08-15 02:12:26 +0100

    update the base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/workspace/LaptopSnipe';
+$config['base_url'] = 'https://laptopsnipe.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
